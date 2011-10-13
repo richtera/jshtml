93a502c16c1fe35276aa84b2719347ae6d7feb36
Elmer Bulthuis <elmerbulthuis@gmail.com>
2011-10-13 23:58:42 +0200

    require test

---
+++ test/requireExamples.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var fs = require('fs');
+var jsHtml = require('../main');
+var util = require('../lib/util');
+
+function runDirectory(dirPath, options)	{
+	try	{
+		options = util.extend({}, options, JSON.parse(fs.readFileSync(dirPath + '.json', 'utf-8')));
+	}
+	catch(ex){}
+	
+	fs.readdirSync(dirPath).forEach(function(subPath) {
+		var filePath = dirPath + '/' + subPath;
+		var fileStat = fs.statSync(filePath);
+		if(fileStat.isDirectory()) runDirectory(filePath, options);
+		if(fileStat.isFile()) runFile(filePath, options);
+	});
+}
+
+function runFile(filePath, options)	{
+	var match = /((.*\/)?(.+))\.jshtml$/i.exec(filePath);
+	if (!match) return;
+
+	console.log('[' + match[3] + ']');
+
+	try	{
+		options = util.extend({}, options, JSON.decode(fs.readFileSync(match[1] + '.json')));
+	}
+	catch(ex){}
+
+	require(match[1]);
+}
+
+runDirectory(path.normalize(__dirname + '/../examples', {}));
+
+
