0ce67cd152c4589f7f1326e9a3c6ac96d3ed9ccf
Elmer Bulthuis <elmerbulthuis@gmail.com>
2011-10-21 13:07:18 +0200

    nog log

--- test/walkWebsites.js
+++ test/walkWebsites.js
@@ -62,7 +62,7 @@ function walkUrl(url, cb)	{
 	zombie.visit(url, function (err, browser, status) {
 		console.log(url + '\t' + status);
 		assert.ifError(err);
-		console.log(browser);
+		//console.log(browser);
 		//console.log(browser.querySelectorAll('a'));
 		cb && cb();		
 	});
